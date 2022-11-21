c94b4ab6556adfda8c975ef14d7213d909a7a0a1
joseiramrj <joseiramrj@gmail.com>
2022-11-21 16:43:17 -0300

    VAU

--- api/downloadfiles/__init__.py
+++ api/downloadfiles/__init__.py
@@ -2,10 +2,7 @@ import logging
 import wget
 from bs4 import BeautifulSoup
 
-
 import azure.functions as func
-
-
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
