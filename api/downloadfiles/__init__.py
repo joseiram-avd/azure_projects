cf02c32e800cc0e49e53e1d9030a67b694e6b5eb
joseiramrj <joseiramrj@gmail.com>
2022-11-21 18:09:31 -0300

    scrapy

--- api/downloadfiles/__init__.py
+++ api/downloadfiles/__init__.py
@@ -1,8 +1,12 @@
 import logging
 import wget
+import scrapy
 from bs4 import BeautifulSoup
 
 import azure.functions as func
+
+
+
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
