ed332f04a6fc010df9c96b7fc476af877ae46e90
joseiramrj <joseiramrj@gmail.com>
2022-11-22 08:02:32 -0300

    vai

--- api/downloadfiles/__init__.py
+++ api/downloadfiles/__init__.py
@@ -11,7 +11,8 @@ import re
  
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
-
+from scrapy.pipelines.files import FilesPipeline
+  
 # Reactor restart
 
 from crochet import setup, wait_for
@@ -24,13 +25,18 @@ class DownfilesItem(scrapy.Item):
 	original_file_name = scrapy.Field()
 	files = scrapy.Field
 
-class NirsoftSpider(scrapy.Spider):
-    name = 'nirsoft'
-    # allowed_domains = ['dados.antt.gov.br']
-    # start_urls = ['https://dados.antt.gov.br/dataset/veiculos-habilitados']
+class DownfilesPipeline(FilesPipeline):
+    def file_path(self, request, response=None, info=None):
+        file_name: str = request.url.split("/")[-1]
+        return file_name
 
-    allowed_domains = ['www.nirsoft.net']
-    start_urls = ['http://www.nirsoft.net/']
+class NirsoftSpider(CrawlSpider):
+    name = 'nirsoft'
+    allowed_domains = ['dados.antt.gov.br']
+    start_urls = ['https://dados.antt.gov.br/dataset/veiculos-habilitados']
+
+    # allowed_domains = ['www.nirsoft.net']
+    # start_urls = ['http://www.nirsoft.net/']
    
     # 'REQUEST_FINGERPRINTER_IMPLEMENTATION': '2.7',
     # scrapy.pipelines.files.FilesPipeline
@@ -38,13 +44,13 @@ class NirsoftSpider(scrapy.Spider):
 
     custom_settings = {
         'ITEM_PIPELINES': {
-            'scrapy.pipelines.files.FilesPipeline': 1
+            'DownfilesPipeline': 1
         },
         'FILES_STORE' : 'C:/nirsoft'
     }
 
     rules = (
-        Rule(LinkExtractor(allow=r'utils/'),
+        Rule(LinkExtractor(allow=r'dataset/'),
         callback='parse_item', follow = True),
     ) 
 
@@ -52,10 +58,10 @@ class NirsoftSpider(scrapy.Spider):
         print( i )
 
     def parse_item(self, response):
-        file_url = response.css('.downloadline::attr(href)').get()
+        file_url = response.css('.resource-url-analytics::attr(href)').get()
         file_url = response.urljoin(file_url)
         file_extension = file_url.split('.')[-1]
-        if file_extension not in ('zip', 'exe', 'msi'):
+        if file_extension not in ('zip', 'json', 'csv'):
             return
         item = DownfilesItem()
         item['file_urls'] = [file_url]
