efb8a73f67219c96fe9c6e0cda60bb783b0b9f6d
joseiramrj <joseiramrj@gmail.com>
2022-11-22 08:24:19 -0300

    vai

--- api/downloadfiles/__init__.py
+++ api/downloadfiles/__init__.py
@@ -19,17 +19,6 @@ from crochet import setup, wait_for
 
 setup()
 
-class DownfilesItem(scrapy.Item):
-	# define the fields for your item here like:
-	file_urls = scrapy.Field()
-	original_file_name = scrapy.Field()
-	files = scrapy.Field
-
-class DownfilesPipeline(FilesPipeline):
-    def file_path(self, request, response=None, info=None):
-        file_name: str = request.url.split("/")[-1]
-        return file_name
-
 class NirsoftSpider(CrawlSpider):
     name = 'nirsoft'
     allowed_domains = ['dados.antt.gov.br']
@@ -44,8 +33,7 @@ class NirsoftSpider(CrawlSpider):
 
     custom_settings = {
         'ITEM_PIPELINES': {
-            '__main__.DownfilesPipeline': 1,
-            'scrapy.pipelines.files.FilesPipeline':10
+            '__main__.DownfilesPipeline': 1
         },
         'FILES_STORE' : 'C:/nirsoft'
     }
@@ -69,6 +57,18 @@ class NirsoftSpider(CrawlSpider):
         item['original_file_name'] = file_url.split('/')[-1]
         yield item
 
+class DownfilesItem(scrapy.Item):
+	# define the fields for your item here like:
+	file_urls = scrapy.Field()
+	original_file_name = scrapy.Field()
+	files = scrapy.Field
+
+class DownfilesPipeline(scrapy.pipelines.files.FilesPipeline):
+    def file_path(self, request, response=None, info=None):
+        print( 'filename' )
+        file_name: str = request.url.split("/")[-1]
+        return file_name
+
  
 def run_spider():
     crawler = CrawlerRunner()
@@ -79,7 +79,7 @@ def run_spider():
 # @wait_for(10)
 def end_spider():
     return func.HttpResponse(
-            "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+            "1 This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
             status_code=200
     )
 
