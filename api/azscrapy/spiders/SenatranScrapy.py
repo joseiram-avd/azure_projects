90112466171e217297bb1960fe95027d78ef3d23
joseiramrj <joseiramrj@gmail.com>
2022-12-28 14:47:51 -0300

    Unficação de Senatran e Ibge

--- api/azscrapy/spiders/SenatranScrapy.py
+++ api/azscrapy/spiders/SenatranScrapy.py
@@ -6,23 +6,29 @@ from azscrapy.items import DownfilesItem
 import os
 from urllib.parse import urlparse
 import logging
+
 from azscrapy.middlewares import AzScrapyCrawlSpiderFiles
 
 
-class Senatran_Frota(AzScrapyCrawlSpiderFiles):
+class SenatranScrapy(AzScrapyCrawlSpiderFiles):
 
     logger = logging.getLogger(__name__)
 
-    name            = 'Senatran_Frota'
+    name            = 'SenatranScrapy'
     allowed_domains = ['gov.br']
-    start_urls      = ['https://dados.transportes.gov.br/dataset/frota-de-veiculos']
-    # folder_name     = "senatran"
+
+    start_urls_frotas      = ['frotas'    , 'https://dados.transportes.gov.br/dataset/frota-de-veiculos']
+    start_urls_condutores  = ['condutores', 'https://www.gov.br/infraestrutura/pt-br/assuntos/transito/conteudo-Senatran/estatisticas-quantidade-de-habilitados-denatran']
+
+    start_urls = [
+        start_urls_frotas,
+        start_urls_condutores
+    ]
 
     meses           = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho', 'julho',
                         'agosto', 'setembro', 'outubro',' novembro', 'dezembro',
                         'jan', 'fev', 'mar', 'abr', 'mai', 'jun', 'jul',
                         'ago', 'set', 'out',' nov', 'dez']
-
 
     def mesNum(self,nome_mes):
         if nome_mes == 'Jan':
@@ -53,20 +59,30 @@ class Senatran_Frota(AzScrapyCrawlSpiderFiles):
         return(mes_num)
 
 
-
     def start_requests(self):
-        for url in self.start_urls:
-            request = scrapy.Request(url, callback=self.parse_link)
-            yield request
+        for item in self.start_urls:
+            tipo = item[0]
+            url = item[1]
+
+            if tipo == 'frotas':
+                request = scrapy.Request(url, callback=self.parse_link_frotas)
+                yield request
+
+            if tipo == 'condutores':
+                request = scrapy.Request(url, callback=self.parse_link_condutores)
+                yield request
 
 
-    def parse_link(self, response):
+    # ************************************************
+    # FROTAS
+    # **************************************************
+    def parse_link_frotas(self, response):
         links = set( LinkExtractor( deny=('dicionario'), canonicalize=True, unique=True).extract_links(response) )
         for link in links:
-            yield scrapy.Request(link.url, callback=self.parse_item, dont_filter=True )
+            yield scrapy.Request(link.url, callback=self.parse_item_frotas, dont_filter=True )
 
-
-    def parse_item(self, response):
+    # FROTAS
+    def parse_item_frotas(self, response):
 
         file_url = response.css('.resource-url-analytics::attr(href)').get()
         file_url = response.urljoin(file_url)
@@ -86,7 +102,32 @@ class Senatran_Frota(AzScrapyCrawlSpiderFiles):
 
         item = DownfilesItem()
         item['file_urls'] = [file_url]
-
         item['original_file_name'] = self._FOLDER_NAME + '/frota_veiculos/' + nome_arquivo
+
+        yield item
+
+    # ************************************************
+    # CONDUTORES
+    # **************************************************
+    def parse_link_condutores(self, response):
+        links = set( LinkExtractor( allow=('condutores'), canonicalize=True, unique=True).extract_links(response) )
+        for link in links:
+            yield scrapy.Request(link.url, callback=self.parse_item_condutores, dont_filter=True )
+
+
+    def parse_item_condutores(self, response):
+
+        file_url = response.css('.resource-url-analytics::attr(href)').get()
+        file_url = response.urljoin(file_url)
+        file_extension = file_url.split('.')[-1]
+
+        if file_extension not in ('csv'):
+            return
+
+        nome_arquivo = file_url.split('/')[-1].replace('-','_')
+
+        item = DownfilesItem()
+        item['file_urls'] = [file_url]
+        item['original_file_name'] = self._FOLDER_NAME + '/condutores_habilitados/' + nome_arquivo
 
         yield item
